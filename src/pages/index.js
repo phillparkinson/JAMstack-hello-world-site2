e6e46c376af65eddb9cf0a7e9e8d6d3259a9993d
pparkinson <phillip.parkinson@sap.com>
2018-08-30 00:47:35 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi Alle</h1>
+    <p>Welcome to Phillip Parkinson's site.</p>
+    <p>I plan to build something great here.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
